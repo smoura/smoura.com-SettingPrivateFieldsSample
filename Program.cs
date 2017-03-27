43c9b63cd360665de9fa8d51f2b6e6c31dbb74f1
Samuel Moura <username@users.noreply.github.com>
2017-03-27 23:00:20 +0100

    Code sample for blog on SettingPrivateFields with Rflection

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace SettingPrivateField
+{
+    using System;
+    using System.Reflection;
+
+    class Program
+    {
+        public class Person
+        {
+            private string name;
+
+            public string Name
+            {
+                get { return name; }
+                private set { name = value; }
+            }
+
+            public Person(string name)
+            {
+                this.name = name;
+            }
+        }
+
+        public static void SetField<T>(T instance, string fieldName, object obj)
+        {
+            var f = typeof(T).GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+            if (obj.GetType() != f.FieldType)
+                throw new InvalidOperationException("Types do not match!");
+            f.SetValue(instance, obj);
+        }
+
+        static void Main(string[] args)
+        {
+            var person = new Person("John");
+            Console.WriteLine(string.Format("My name is {0}!", person.Name));
+            SetField(person, "name", "Jason");
+            Console.WriteLine(string.Format("My name is {0}!", person.Name));
+
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # smoura.com-SettingPrivateFieldsSample
 Setting a Private Field on an instance of an object using Reflection
+
+This is the source code for an old article published in my blog on the following link: http://smoura.com/setting-a-private-field-on-an-instance-of-an-object-using-reflection/
